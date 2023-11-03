a9077d6c20c54020424d0e0a75e0c793ef663edf
Pranto Sheikh <prantosheikh834@gmail.com>
2023-11-03 19:06:39 +0600

    Sidebar basic setup completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import './App.css'
 import Navigation from './Navigation/Nav'
 import Products from './Products/Products'
 import Recommended from './Recommended/Recommended'
+import Sidebar from './Sidebar/Sidebar'
 
 function App() {
 
 
   return (
     <>
+    <Sidebar/>
      <Navigation/>
      <Recommended/>
      <Products />
---
+++ src/Sidebar/Sidebar.jsx
@@ -0,0 +1,19 @@
+import Category from "./Category/Category";
+import Color from "./Colors/Colors";
+import Price from "./Price/Price";
+import "./Sidebar.css";
+const Sidebar = () => {
+  return (
+    <section className="sidebar">
+      <div className="logo-container">
+        <h1>Logo</h1>
+      </div>
+
+      <Category />
+      <Price />
+      <Color />
+    </section>
+  );
+};
+
+export default Sidebar;
