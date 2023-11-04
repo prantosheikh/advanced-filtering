796558ce0bb2262f6d5de077847ff788a754c1fb
Pranto Sheikh <prantosheikh834@gmail.com>
2023-11-04 21:55:30 +0600

    color filter functionally

--- src/Sidebar/Colors/Colors.jsx
+++ src/Sidebar/Colors/Colors.jsx
@@ -1,14 +1,47 @@
 import Input from "../../components/Input";
 import "./Colors.css";
 
-const Colors = () => {
+const Colors = ({handleChange}) => {
   return (
     <div className="">
       <h2 className="sidebar-title color-title">Color</h2>
-      <Input/>
-      <Input/>
-      <Input/>
-      <Input/>
+      <label className="sidebar-label-container">
+        <input onChange={handleChange} type="radio" value="" name="test1" />
+        <span className="checkmark all"></span>All
+      </label>
+      <Input
+            handleChange={handleChange}
+            value="black"
+            title="Black"
+            name="test1"
+            color= 'black'
+          />
+      <Input
+            handleChange={handleChange}
+            value="blue"
+            title="Blue"
+            name="test1"
+            color= 'blue'
+          />
+      <Input
+            handleChange={handleChange}
+            value="red"
+            title="Red"
+            name="test1"
+            color= 'red'
+          />
+      <Input
+            handleChange={handleChange}
+            value="green"
+            title="Green"
+            name="test1"
+            color= 'green'
+          />
+      <label className="sidebar-label-container">
+        <input handleChange={handleChange} value='white' name="test1" type="radio" />
+        <span className="checkmark"></span> 
+        White
+      </label>
     </div>
   );
 };
